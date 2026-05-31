61aa9e08771863b0d265ed99c6fca78b51462aeb
sarvajeet-dev <sarvajeet2401@gmail.com>
2026-05-31 16:38:24 +0530

    created jwt token

---
+++ utils/jwt.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken"
+
+export const generateToken = (user) =>{
+   return jwt.sign(
+    {
+        id: user._id,
+        role : user.role,
+        organisationId : user.organisationId
+
+    } , 
+    process.env.JWT_SECRET , 
+    {
+        expiresIn : "15m"
+    }
+   )
+}
+
+export const refreshToken = (user) => {
+    return jwt.sign({
+        id : user._id
+
+    },
+    process.env.JWT_SECRET,
+    {
+        expiresIn : "7d"
+    }
+)
+}
